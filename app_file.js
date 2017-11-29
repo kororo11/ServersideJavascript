256fef256c290529ffe46148904d28dd5710b4c5
kororo11 <kororo11@naver.com>
2017-11-29 09:27:36 +0900

    writeFile
    '/topic/new' 에서 data 폴더에 writeFile 되는 부분까지 했음

--- app_file.js
+++ app_file.js
@@ -1,8 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
 var app = express();
 
-app.get('/topic', function (req, res) {
-  res.send('Hello');
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.locals.pretty = true;
+app.set('views', './view_file');
+app.set('view engine', 'jade');
+
+app.get('/topic/new', function (req, res) {
+  res.render('new');
+});
+
+app.post('/topic', function (req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error!');
+    }
+    res.send('Success!!!');
+  })
 });
 
 app.listen(3000,function () {
